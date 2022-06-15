aee0ad909e2d9f65d286371e1137eb552d9ef4a6
Paul Thrasher <thrashr888@gmail.com>
2022-06-15 16:24:58 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
 variable "bucket_name" {
   default = "drift-bucket"
 }
